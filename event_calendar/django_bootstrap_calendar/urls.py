361931889c5dd69ff8d113fa017d44a9176672dc
Cossette Alexis Gabuya <gabuyalexis@gmail.com>
2023-05-27 18:29:16 +0800

    #4
    WIP fix calendar rendering

--- event_calendar/django_bootstrap_calendar/urls.py
+++ event_calendar/django_bootstrap_calendar/urls.py
@@ -5,14 +5,6 @@ from django.urls import path
 from django_bootstrap_calendar.views import CalendarJsonListView, CalendarView
 
 urlpatterns = [
-    path(
-        r'^json/$',
-        CalendarJsonListView.as_view(),
-        name='calendar_json'
-    ),
-    path(
-        r'^$',
-        CalendarView.as_view(),
-        name='calendar'
-    ),
+    path('json/', CalendarJsonListView.as_view(), name='calendar_json'),
+    path('^$', CalendarView.as_view(), name='calendar'),
 ]
